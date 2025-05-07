7fa697e4bda3f18140b152cf549e79cd5a3d4b93
mkychsu <mkychsu@eofe7.mit.edu>
2025-05-07 14:21:57 -0400

    update for together api

---
+++ Notebooks/SG/job_KGs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name="KGs"
+###SBATCH --partition=sched_mit_buehler
+#SBATCH --partition=sched_mit_buehler_gpu
+#SBATCH --gres=gpu:4
+
+#SBATCH -N 1
+#SBATCH -n 8
+
+#SBATCH --mem-per-cpu=4G
+
+#SBATCH --time=12:0:0
+#SBATCH --output=cout.txt
+#SBATCH --error=cerr.txt
+###SBATCH --nodelist=node1230
+
+module purge
+source ~/.bashrc
+source ~/ml.sh
+rm core*
+#conda deactivate
+conda activate llm
+
+
+for (( c=0; c <= 4000; ++c ))
+do
+  if (( $c%$1 == $2))
+  then
+    continue
+  fi
+  python make_KGs_SG.py $c
+done
+
+
+
---
+++ Notebooks/SG/job_mainKG.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name="mainKG"
+###SBATCH --partition=sched_mit_buehler
+#SBATCH --partition=sched_mit_buehler_gpu
+#SBATCH --gres=gpu:2
+
+###SBATCH -N 8
+###SBATCH -n 8
+
+#SBATCH --mem-per-cpu=16G
+
+#SBATCH --time=12:0:0
+#SBATCH --output=cout_main.txt
+#SBATCH --error=cerr_main.txt
+###SBATCH --nodelist=node1230
+
+module purge
+source ~/.bashrc
+source ~/ml.sh
+rm core*
+#conda deactivate
+conda activate llm
+
+python make_mainKGs_SG.py
+
+
